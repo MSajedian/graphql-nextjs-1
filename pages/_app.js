c3d40ab7e5006259994942748e5551b8842aabd2
MSajedian <mohammadsajedian@gmail.com>
2022-07-04 12:08:17 +0100

    added ApolloClient

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
+import { ApolloClient, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/graphql",
+});
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ApolloProvider client={client}>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  )
 }
 
 export default MyApp
